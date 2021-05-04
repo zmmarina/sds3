14ce32e3451d31866e1d60d886c5341ba3bd14d1
zmmarina <zmarina.hm@gmail.com>
2021-05-04 01:22:22 -0300

    Static graphics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
 import NavBar from 'components/NavBar';
 import Footer from 'components/Footer';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
 
 function App() {
   return (
     <>
     <NavBar/>
     <div className="container">
-      <h1 className="text-primary">Olá mundo</h1>
+      <h1 className="text-primary py-3">Instructors Dashboard</h1>
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Instructor's Info</h5>
+          <BarChart/>
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Instructor's Info</h5>
+          <DonutChart/>
+        </div>
+      </div>
+
+      <div className="py-3">
+        <h2 className="text-primary">Quantity of Classes</h2>
+      </div>
       <DataTable/>
     </div>
     <Footer/>
