0151c4500168270c3aa61cae74e2337362a55ec1
zmmarina <zmarina.hm@gmail.com>
2021-05-03 22:56:39 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/zm/ZmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.zm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/zm/ZmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.zm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
