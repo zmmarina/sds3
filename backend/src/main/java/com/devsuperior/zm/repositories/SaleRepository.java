693312a6455d2c2be45dc97c98655bfcea24c800
zmmarina <zmarina.hm@gmail.com>
2021-05-07 23:36:49 -0300

    pagination

---
+++ backend/src/main/java/com/devsuperior/zm/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.zm.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.zm.entities.Sale;
+
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/devsuperior/zm/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.zm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.zm.dto.SaleDTO;
+import com.devsuperior.zm.entities.Sale;
+import com.devsuperior.zm.repositories.SaleRepository;
+import com.devsuperior.zm.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+}
