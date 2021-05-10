6cb4f25a06baf6d6f1e643d24a332abf83b85192
zmmarina <zmarina.hm@gmail.com>
2021-05-09 23:03:45 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,8 @@
-import NavBar from 'components/NavBar';
-import Footer from 'components/Footer';
-import DataTable from 'components/DataTable';
-import BarChart from 'components/BarChart';
-import DonutChart from 'components/DonutChart';
+import Routes from 'Routes';
 
 function App() {
   return (
-    <>
-    <NavBar/>
-    <div className="container">
-      <h1 className="text-primary py-3">Instructors Dashboard</h1>
-      <div className="row px-3">
-        <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Instructor's Info</h5>
-          <BarChart/>
-        </div>
-        <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Quantity of Classes</h5>
-          <DonutChart/>
-        </div>
-      </div>
-
-      <div className="py-3">
-        <h2 className="text-primary">All classes</h2>
-      </div>
-      <DataTable/>
-    </div>
-    <Footer/>
-    </>
+   <Routes />
   );
 }
 
